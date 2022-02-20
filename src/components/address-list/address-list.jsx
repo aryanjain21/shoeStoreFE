89441c94c2394ca81de9da34c6b638bbea4faa9e
aryanjain21 <aryanjain1997@gmail.com>
2022-02-20 18:10:21 +0530

    Updated address list component

--- src/components/address-list/address-list.jsx
+++ src/components/address-list/address-list.jsx
@@ -1,9 +1,18 @@
 import Button from '../../common/button/button';
 import './address-list.scss';
+import { deleteAddress } from '../../services';
 
 const AddressList = (props) => {
 
-    const { addressData, handleRemove, handleEdit } = props;
+    const { addressData, handleEdit, fetchAddressList } = props;
+
+    const handleRemove = (addressId) => {
+        deleteAddress({addressId: addressId}).then(resp => {
+            if(resp.data.status === 200) {
+                fetchAddressList();
+            }
+        })
+    }
 
     return (
         addressData.loader ? <h4>Loading....</h4> :
@@ -11,16 +20,16 @@ const AddressList = (props) => {
                 addressData.addressList.map((address, index) => {
                     return <div className='address_list_container' key={index}>
                         <div className='top_section'>
-                            <div className='person_name'>Name</div>
-                            <div className='mobile_number'>9876543210</div>
+                            <div className='person_name'>{address.name}</div>
+                            <div className='mobile_number'>{address.mobileNumber}</div>
                         </div>
                         <div className='address_section'>
-                            <div className='street'>{address.line1} - {address.city}</div>
-                            <div className='area'>{address.state} - {address.postal_code}</div>
+                            <div className='street'>{address.street} {address.landmark} - {address.city}</div>
+                            <div className='area'>{address.state} - {address.pincode}</div>
                         </div>
                         <div className='btn_section'>
                             <div className='edit_btn'><Button clickHandler={() => handleEdit(address)}>Edit</Button></div>
-                            <div className='remove_btn'><Button>Remove</Button></div>
+                            <div className='remove_btn'><Button clickHandler={() => handleRemove(address._id)}>Remove</Button></div>
                         </div>
                     </div>
                 })

ead8e7866e0f37a66fada9197562bcd289fbf184
aryanjain21 <aryanjain1997@gmail.com>
2022-02-27 16:40:28 +0530

    Updated product card

--- src/common/product-card/product-card.jsx
+++ src/common/product-card/product-card.jsx
@@ -1,34 +1,85 @@
 import './product-card.scss';
+import { useNavigate } from 'react-router-dom';
 import Button from '../button/button';
-import Shoe from '../../assets/images/shoe1.jpg';
-import Buy from '../../assets/icons/buy.svg';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import FillHeart from '../../assets/icons/fill_heart.svg';
+import { moveToCart } from '../../services';
 
 const ProductCard = (props) => {
 
-    const { products } = props;
+    const { products = [], hidebtn, isCarousel, isWhishlist, removeProduct, fetchWishlist, fetchCartList } = props;
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (id) => {
+        if (isWhishlist) {
+            navigate(`/buy/${id}`);
+        } else {
+            navigate(`/product`);
+        }
+    }
+
+    const handleBtnClick = (id) => {
+        if (isWhishlist) {
+            moveToCart({productId: id}).then(resp => {
+                fetchWishlist();
+                fetchCartList();
+            })
+        } else {
+            navigate(`/buy/${id}`)
+        }
+    }
 
     return (
-        <div className='product_card_container'>
-            {(products.length > 0) && products.map((product, index) => (<div className='product_wrapper'>
-                <div className='product_image_section'>
-                    <img src={Shoe} alt="product" />
-                </div>
-                <div className='product_discription'>
-                    <div className='product_details'>
-                        <div className='wishlist_area'><img src={Buy} alt="buy" /></div>
-                        <div className='title'>{product.name}</div>
-                        <div className='price_section'>
-                            <div className='original_price'>MRP <span>2000</span></div>
-                            <div className='discount_price'>{product.price} <span>(incl. all tax)</span></div>
+        <div className={`product_card_container ${isWhishlist ? 'wishlist_data' : ''}`}>
+            {isCarousel ?
+                <Carousel showArrows={true} centerMode={true} width='60%' autoPlay={true} swipeScrollTolerance selectedItem={3}>
+                    {(products.length > 0) && products.map((product, index) => {
+                        return <div className={`product_wrapper ${hidebtn ? 'product_without_btn' : ''}`} key={index}>
+                            <div className='product_image_section' onClick={() => handleNavigation(product._id)}>
+                                <img src={product.image} alt="product" />
+                            </div>
+                            <div className='product_discription'>
+                                <div className='product_details'>
+                                    <div className='brand'>{product.brand}</div>
+                                    <div className='title' onClick={() => handleNavigation(product._id)}>{product.title}</div>
+                                    <div className='price_section'>
+                                        <div className='discount_price'><span className='strike_through'>MRP ₹{product.price}</span> <span className='discount'>({product.discount}% off)</span></div>
+                                        <div className='discount_price mrp_price'>Rs. {(product.price - (product.price * (product.discount / 100))).toFixed(2)}</div>
+                                    </div>
+                                </div>
+                                {!hidebtn && <div className='btn_area'>
+                                    <Button clickHandler={() => { navigate(`/buy/${product._id}`) }}>Buy Now</Button>
+                                </div>}
+                            </div>
                         </div>
+                    })}
+                </Carousel>
+                :
+                (products.length > 0) && products.map((product, index) => (<div className={`product_wrapper ${hidebtn ? 'product_without_btn' : ''}`} key={index}>
+                    <div className='product_image_section' onClick={() => handleNavigation(product._id)}>
+                        <img src={product.image} alt="product" />
                     </div>
-                    <div className='btn_area'>
-                        <Button>Buy Now <span className='buy_icon'><img src={Buy} alt="buy" /></span></Button>
+                    <div className='product_discription'>
+                        <div className='product_details'>
+                            {isWhishlist && <div className='wished_icon' onClick={() => removeProduct({ productId: product._id })}>
+                                <img src={FillHeart} alt="filled" />
+                            </div>}
+                            <div className='brand'>{product.brand}</div>
+                            <div className='title' onClick={() => handleNavigation(product._id)}>{product.title}</div>
+                            <div className='price_section'>
+                                <div className='discount_price'><span className='strike_through'>MRP ₹{product.price}</span> <span className='discount'>({product.discount}% off)</span></div>
+                                <div className='discount_price mrp_price'>Rs. {(product.price - (product.price * (product.discount / 100))).toFixed(2)}</div>
+                            </div>
+                        </div>
+                        {!hidebtn && <div className='btn_area'>
+                            <Button clickHandler={() => handleBtnClick(product._id)}>{isWhishlist ? 'Move to Cart' : 'Buy Now'}</Button>
+                        </div>}
                     </div>
-                </div>
-            </div>)
-            )}
-        </div>
+                </div>)
+                )}
+        </div >
     );
 }
 
